e55e1afe1dae6c6b9ba1dbc00ac3e1a3424105ea
Aitor Roca <aitorrocaromero@gmail.com>
2017-08-27 13:30:11 +0200

    Now works

--- src/getProductsByIds.m
+++ src/getProductsByIds.m
@@ -3,20 +3,23 @@ function [ ProductsByIds ] = getProductsByIds( session, ids )
 %ids = ["13519974", "13585545"];
 load globalConstants;
 
-options = weboptions(...
-     'HeaderFields',{'Content-Type' 'application/json'},...
-     'RequestMethod','POST','ArrayFormat','json');
-options.MediaType = 'application/json';
-data = struct(...
-    'ids', ids);
-
 URL = matlab.net.URI(BASE_TRADER_URL);
 URL.Path = '/product_search/secure/v5/products/info'; 
 URL.Query = struct(...
     'intAccount',int2str(session.account),...
     'sessionId',session.id);
-%jsonencode(data)
-ProductsByIds = webwrite(char(URL), data, options);
+
+options = weboptions(...
+     'HeaderFields',{
+     'Connection' 'keep-alive';
+     'Origin' 'https://trader.degiro.nl';
+     'Content-Type' 'application/json;charset=UTF-8';
+     'referrer' 'https://trader.degiro.nl/trader';
+     'Cookie' strcat('JSESSIONID=',session.id)},...
+     'RequestMethod','POST','ArrayFormat','csv');
+
+options.MediaType = 'application/json';
+
+ProductsByIds = webwrite(char(URL), ids, options);
 
 end
-
